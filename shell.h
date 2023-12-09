6fb4d91b90d7d765d726b4296ca22f9ab00948d9
kc-clintone <kayseeclintone@gmail.com>
2023-12-09 23:49:25 +0300

    added the shell.h file

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef _SHELL_
+#define _SHELL_
+
+/** some important includes **/
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <dirent.h>
+#include <signal.h>
+#include <stdio.h>
