1355ec21ff116a5aa2ff1678bfe6e69656b24813
kc-clintone <kayseeclintone@gmail.com>
2024-03-01 18:04:38 +0000

    simple_shell >>> corrections

--- 0-basic_mode.c
+++ 0-basic_mode.c
@@ -1,20 +1,6 @@
 #include "shell.h"
 
 /**
- * _control_c - This simple fn handles interupt for the
- * CTRL+C signal.
- * @sgn: The signal value.
- * Return: Nada.
-*/
-void _control_c(int sgn)
-{
-if (sgn == SIGINT)
-{
-custom_printf("\n$ ", STDIN_FILENO);
-}
-}
-
-/**
  * basic_shell - This function handles the basic part of the shell.
  * Return: Nada.
 */
