2773a5eb8f92e174f0d1886017000634032a7e75
kc-clintone <kayseeclintone@gmail.com>
2024-03-01 20:43:11 +0000

    simple_shell >>> corrections

--- 1-envs.c
+++ 1-envs.c
@@ -1,6 +1,6 @@
 #include "shell.h"
 /**
- * _env - This will print the the environment of the user in session.
+ * xenv - This will print the the environment of the user in session.
  * @x_cmd: A command parameter.
  * Return: Nada.
 */
@@ -8,9 +8,9 @@ void xenv(char **x_cmd __attribute__((unused)))
 {
 int x;
 
-for (x = 0; environ[count] != NULL; count++)
+for (x = 0; _genv[x] != NULL; x++)
 {
-custom_printf(environ[count], STDOUT_FILENO);
-custom_printf("\n", STDOUT_FILENO);
+_stdout(_genv[x], STDOUT_FILENO);
+_stdout("\n", STDOUT_FILENO);
 }
 }
