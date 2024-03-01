11a217c2ce560abfc4f8b0fa8ce9c1360615edc7
kc-clintone <kayseeclintone@gmail.com>
2024-03-01 13:45:12 +0000

    simple_shell >>> corrections

---
+++ 0-basic_mode.c
@@ -0,0 +1,51 @@
+#include "shell.h"
+
+/**
+ * _control_c - This simple fn handles interupt for the
+ * CTRL+C signal.
+ * @sgn: The signal value.
+ * Return: Nada.
+*/
+void _control_c(int sgn)
+{
+if (sgn == SIGINT)
+{
+custom_printf("\n$ ", STDIN_FILENO);
+}
+}
+
+/**
+ * basic_shell - This function handles the basic part of the shell.
+ * Return: Nada.
+*/
+void basic_shell(void)
+{
+int x, t_cmd = 0;
+char **active_simple_cmd = NULL;
+size_t i = 0;
+
+if (!(isatty(STDIN_FILENO)))
+{
+while (getline(&cmd_line, &i, stdin) != -1)
+{
+remove_newline_character(cmd_line);
+ignore_comment_str(cmd_line);
+parsed_cmds = token_processor(cmd_line, ";");
+for (x = 0; parsed_cmds[x] != NULL; x++)
+{
+active_simple_cmd = token_processor(parsed_cmds[x], " ");
+if (active_simple_cmd[0] == NULL)
+{
+free(active_simple_cmd);
+break;
+}
+t_cmd = parse_processed_cmds(active_simple_cmd[0]);
+shell_init_func(active_simple_cmd, t_cmd);
+free(active_simple_cmd);
+}
+free(parsed_cmds);
+}
+free(cmd_line);
+exit(status);
+}
+}
