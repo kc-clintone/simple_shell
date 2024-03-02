49a1ad973952edcd8e5bbc25947791ff0405232a
kc-clintone <kayseeclintone@gmail.com>
2024-03-02 08:12:49 +0000

    simple_shell >>> corrections

---
+++ 21-realloc.c
@@ -0,0 +1,43 @@
+#include "shell.h"
+/**
+ * _realloc - This realloc function reallocates memory block.
+ * @ptr: Pointer to the initial memory.
+ * @i: Size of ptr.
+ * @j: Size of new memory to allocate.
+ * Return: Pointer to address of new memory block.
+*/
+void *_realloc(void *ptr, unsigned int i, unsigned int j)
+{
+void *tbfr;
+unsigned int x;
+
+if (ptr == NULL)
+{
+tbfr = malloc(j);
+return (tbfr);
+}
+else if (j == i)
+{
+return (ptr);
+}
+else if (j == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+else
+{
+tbfr = malloc(j);
+if (tbfr != NULL)
+{
+for (x = 0; x < min(i, j); x++)
+{
+*((char *)tbfr + x) = *((char *)ptr + x);
+}
+free(tbfr);
+return (tbfr);
+}
+else
+return (NULL);
+}
+}
