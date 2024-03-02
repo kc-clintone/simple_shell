afa922a9fbd0ffa009a41e35ed128b7e62923a9e
kc-clintone <kayseeclintone@gmail.com>
2024-03-02 07:05:55 +0000

    simple_shell >>> corrections

---
+++ 13-parser.c
@@ -0,0 +1,35 @@
+#include "shell.h"
+/**
+ * parser - This function identifies and parses cmds.
+ * @xcmd: Target cmds.
+ * Description - Utilizes lots of functions to do it's thing.
+ * Return: A constant to the type of cmd.
+*/
+int parseer(char *xcmd)
+{
+int x;
+char *t_path = NULL;
+char *sys_cmd[] = {"env", "exit", NULL};
+
+for (x = 0; xcmd[x] != '\0'; x++)
+{
+if (xcmd[x] == '/')
+{
+return (_EXTCMD);
+}
+}
+for (x = 0; sys_cmd[x] != NULL; x++)
+{
+if (_strcmp(xcmd, sys_cmd[x]) == 0)
+{
+return (_INTCMD);
+}
+}
+t_path = valid_path_check(xcmd);
+if (t_path != NULL)
+{
+free(t_path);
+return (_PATH);
+}
+return (_BADCMD);
+}
